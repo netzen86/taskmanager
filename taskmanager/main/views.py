b9bdfc17b4fc77bbcbb9b8b9a28803afd4ad2a08
netzen86 <netzen0su@gmail.com>
2022-02-16 22:59:13 +0500

    Complete simple Task Manager

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . models import Task
+from . forms import TaskForm
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    # tasks = Task.objects.all()
+    # tasks = Task.objects.order_by('-id')[:1]
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
